a326779be800eb781405509c83b55db34e16cdbb
Sonal <sonalchy02@gmail.com>
2024-04-08 05:06:12 +0530

    update

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -19,47 +19,46 @@ function Footer({ title }) {
     },
   ];
 
-  const backToTop = () =>{
-    window.scrollTo({top:0 , behavior : "smooth"})
-  }
+  const backToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <div className="w-full">
-
-    <div className="bg-indigo-600 py-3 flex items-center justify-center cursor-pointer" onClick={backToTop}>
+      <div
+        className="bg-indigo-600 py-3 flex items-center justify-center cursor-pointer"
+        onClick={backToTop}
+      >
         <span className="text-lg text-white">Back to Top</span>
-    </div>
+      </div>
 
       <div className="links py-4 bg-indigo-700">
-        <ul className="flex items-center justify-center gap-4 ">
-          <li className="px-3 py-1 border-r border-white">
+        <ul className="flex flex-wrap justify-center gap-4">
+          <li className="px-3 py-1 border-b border-white md:border-r md:border-b-0">
             <a href="/" className="text-white text-lg hover:underline">
               Home
             </a>
           </li>
-          <li className="px-3 py-1 border-r border-white">
-            <a
-              href="/about"
-              className="text-white text-lg hover:underline"
-            >
+          <li className="px-3 py-1 border-b border-white md:border-r md:border-b-0">
+            <a href="/about" className="text-white text-lg hover:underline">
               About Us
             </a>
           </li>
-          <li className="px-3 py-1 border-r border-white">
+          <li className="px-3 py-1 border-b border-white md:border-r md:border-b-0">
             <a href="/testing" className="text-white text-lg hover:underline">
               Services
             </a>
           </li>
-          <li className="px-3 py-1 border-r border-white">
-            <a href="/news-events" className="text-white text-lg hover:underline">
+          <li className="px-3 py-1 border-b border-white md:border-r md:border-b-0">
+            <a
+              href="/news-events"
+              className="text-white text-lg hover:underline"
+            >
               News & Events
             </a>
           </li>
-          <li className="px-3 py-1">
-            <a
-              href="/contact"
-              className="text-white text-lg hover:underline"
-            >
+          <li className="px-3 py-1 border-b border-white md:border-b-0">
+            <a href="/contact" className="text-white text-lg hover:underline">
               Contact Us
             </a>
           </li>
@@ -70,9 +69,9 @@ function Footer({ title }) {
         <ul className="flex items-center justify-center gap-4 ">
           {links.map((item) => {
             return (
-              <li className="px-3 py-1 " key={item.id}>
+              <li className="px-3 py-1" key={item.id}>
                 <a href={item.href} target="_blank" className="text-white">
-                  <i className={`fa-brands fa-${item.link} text-2xl`}></i>
+                  <i className={`fab fa-${item.link} text-2xl`}></i>
                 </a>
               </li>
             );
@@ -81,7 +80,7 @@ function Footer({ title }) {
       </div>
 
       <div className="flex items-center justify-center py-6 bg-indigo-900">
-        <span className="text-white text-lg ">
+        <span className="text-white text-lg">
           Copyright @ 2024 {title} GNDEC
         </span>
       </div>
